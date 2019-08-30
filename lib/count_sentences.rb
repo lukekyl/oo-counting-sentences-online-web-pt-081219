26cb923d2d1907cebec92262fddedfb563f7cf67
Kyle Luke <th.oldschool@gmail.com>
2019-08-30 05:06:19 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    array = self.split(/\b((?!=|\.|\?|\!).)+\s\S*\b/)
+    array = self.squeeze("!").squeeze(".")
     array.count
   #binding.pry
   end

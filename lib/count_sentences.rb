410a339da5cc57fda900d5ce0a739f045a8662a9
Kyle Luke <th.oldschool@gmail.com>
2019-08-30 04:21:41 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    array = self.split(/([a-z]\?|[a-z]\.|[a-z]!)/)
+    array = self.split(/([a-z]*\?|[a-z]*\.|[a-z]*!)/)
     binding.pry
     return array.length
       
